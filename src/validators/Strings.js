6b53c384821d0f933065cb4cd832b65558ed6311
isneezy <vilanculoivan@gmail.com>
2017-08-22 11:39:51 +0200

    code refactor and formating

--- src/validators/Strings.js
+++ src/validators/Strings.js
@@ -1,6 +1,24 @@
 import Error from './ValidatorError'
 
 export default {
+  min(name, value, params) {
+    value = value === null || typeof value === 'undefined' ? '' : value
+    return new Promise((resolve, reject) => {
+      var min = parseInt(params[0])
+      if (value.length < min) reject(new Error(name, `${name} field must have at least ${min} chars in length`))
+      else resolve()
+    })
+  },
+
+  max(name, value, params) {
+    value = value === null || typeof value === 'undefined' ? '' : value
+    return new Promise((resolve, reject) => {
+      var max = parseInt(params[0])
+      if (value.length > max) reject(new Error(name, `${name} field must not pass ${max} chars in length`))
+      else resolve()
+    })
+  },
+
   email(name, value) {
     value = value === null || typeof value === 'undefined' ? '' : value
     return new Promise((resolve, reject) => {
