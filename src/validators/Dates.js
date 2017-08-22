64429fa6d1b316089f1d6f4998add9b0fa51573f
isneezy <vilanculoivan@gmail.com>
2017-08-22 09:40:36 +0200

    refactor, code cleanup and formating

--- src/validators/Dates.js
+++ src/validators/Dates.js
@@ -1,54 +1,55 @@
-import dateFn from '../vendor/date';
-import Error from "./ValidatorError";
+import dateFn from '../vendor/date'
+import Error from './ValidatorError'
+
 export default {
-    date (name, value, params, options) {
-        return new Promise((resolve, reject) => {
-            if(dateFn.isDate(value, options.dateFormat)) resolve();
-            reject(new Error(name, `${name} field is not a valid date`));
-        })
-    },
+  date(name, value, params, options) {
+    return new Promise((resolve, reject) => {
+      if (dateFn.isDate(value, options.dateFormat)) resolve()
+      reject(new Error(name, `${name} field is not a valid date`))
+    })
+  },
 
-    after (name, value, params, options) {
-        return new Promise((resolve, reject) => {
-            var dateFormat = options.dateFormat;
-            var date = dateFn.getDateFromFormat(value, dateFormat);
-            var after = dateFn.getDateFromFormat(params[0], dateFormat);
-            if(date > after) {
-                resolve();
-            }
-            reject(new Error(name, `${name} field should be after ${params[0]}`));
-        })
-    },
+  after(name, value, params, options) {
+    return new Promise((resolve, reject) => {
+      var dateFormat = options.dateFormat
+      var date = dateFn.getDateFromFormat(value, dateFormat)
+      var after = dateFn.getDateFromFormat(params[0], dateFormat)
+      if (date > after) {
+        resolve()
+      }
+      reject(new Error(name, `${name} field should be after ${params[0]}`))
+    })
+  },
 
-    after_or_equal (name, value = '', params = [], options = {}) {
-        return new Promise((resolve, reject) => {
-          const dateFormat = options.dateFormat
-          const date = dateFn.getDateFromFormat(value, dateFormat)
-          const after = dateFn.getDateFromFormat(params[0], dateFormat)
-          if( date >= after ) {
-              resolve()
-          } else {
-              reject(new Error(name, `${name} field should be equal or greater than ${params[0]}`))
-          }
-        })
-    },
+  after_or_equal(name, value = '', params = [], options = {}) {
+    return new Promise((resolve, reject) => {
+      const dateFormat = options.dateFormat
+      const date = dateFn.getDateFromFormat(value, dateFormat)
+      const after = dateFn.getDateFromFormat(params[0], dateFormat)
+      if (date >= after) {
+        resolve()
+      } else {
+        reject(new Error(name, `${name} field should be equal or greater than ${params[0]}`))
+      }
+    })
+  },
 
-    before (name, value = '', params = [], options = {}) {
-      return new Promise((resolve, reject) => {
-        if (!(dateFn.isDate(value, options.dateFormat) && dateFn.isDate(params[0], options.dateFormat))) resolve()
-        const date = dateFn.getDateFromFormat(value, options.dateFormat)
-        const before = dateFn.getDateFromFormat(params[0], options.dateFormat)
-        if(date < before) resolve()
-        reject(new Error(name, `${name} field must be a date before ${params[0]}`))
-      })
-    },
-
-  before_or_equal (name, value = '', params = [], options = {}) {
+  before(name, value = '', params = [], options = {}) {
     return new Promise((resolve, reject) => {
       if (!(dateFn.isDate(value, options.dateFormat) && dateFn.isDate(params[0], options.dateFormat))) resolve()
       const date = dateFn.getDateFromFormat(value, options.dateFormat)
       const before = dateFn.getDateFromFormat(params[0], options.dateFormat)
-      if(date <= before) resolve()
+      if (date < before) resolve()
+      reject(new Error(name, `${name} field must be a date before ${params[0]}`))
+    })
+  },
+
+  before_or_equal(name, value = '', params = [], options = {}) {
+    return new Promise((resolve, reject) => {
+      if (!(dateFn.isDate(value, options.dateFormat) && dateFn.isDate(params[0], options.dateFormat))) resolve()
+      const date = dateFn.getDateFromFormat(value, options.dateFormat)
+      const before = dateFn.getDateFromFormat(params[0], options.dateFormat)
+      if (date <= before) resolve()
       reject(new Error(name, `${name} field must be a date before or equal ${params[0]}`))
     })
   }
--- src/validators/Strings.js
+++ src/validators/Strings.js
@@ -1,15 +1,25 @@
 import Error from './ValidatorError'
+
 export default {
-  alpha (name, value) {
+  email(name, value) {
+    value = value === null || typeof value === 'undefined' ? '' : value
     return new Promise((resolve, reject) => {
-      const re = /^[A-z]+$/;
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (re.test(value)) resolve()
+      reject(new Error(name, `${name} is not a valid email`))
+    })
+  },
+
+  alpha(name, value) {
+    return new Promise((resolve, reject) => {
+      const re = /^[A-z]+$/
       if (re.test(value)) resolve()
       const error = new Error(name, `${name} field only allows alphabet`)
       reject(error)
     })
   },
 
-  alpha_dash (name, value) {
+  alpha_dash(name, value) {
     return new Promise((resolve, reject) => {
       const re = /^[a-zA-Z0-9-_]+$/
       if (re.test(value)) resolve()
@@ -18,7 +28,7 @@ export default {
     })
   },
 
-  alpha_numeric (name, value) {
+  alpha_numeric(name, value) {
     return new Promise((resolve, reject) => {
       const re = /^[a-zA-Z0-9]*$/
       if (re.test(value)) resolve()
@@ -32,12 +42,12 @@ export default {
       const min = parseInt(params[0])
       const max = parseInt(params[1])
       let length = value.length
-      if(length > min && length < max) resolve()
+      if (length > min && length < max) resolve()
       reject(new Error(name, `${name} field must be between ${min} and ${max}`))
     })
   },
 
-  url (name, value) {
+  url(name, value) {
     return new Promise((resolve, reject) => {
       // regex from @diegoperini see https://mathiasbynens.be/demo/url-regex and https://gist.github.com/dperini/729294
       const re = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i
