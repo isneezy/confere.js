995064b35b8d60b93882f85aa69991141c55b912
isneezy <vilanculoivan@gmail.com>
2017-08-22 11:40:10 +0200

    add digits validator

--- src/validators/Numbers.js
+++ src/validators/Numbers.js
@@ -1,24 +1,10 @@
 import Error from './ValidatorError'
 
 export default {
-  min(name, value, params) {
-    value = value === null || typeof value === 'undefined' ? '' : value
+  digits(name, value, params = []) {
     return new Promise((resolve, reject) => {
-      var min = parseInt(params[0])
-      if (value.length < min) reject(new Error(name, `${name} field must have at least ${min} chars in length`))
-      else resolve()
+      if (/^\d+$/.test(value) && String(value).length == parseInt(params[0])) resolve()
+      reject(new Error(name, `${name} field must be digits with size = ${params[0]}`))
     })
-  },
-
-  max(name, value, params) {
-    value = value === null || typeof value === 'undefined' ? '' : value
-    return new Promise((resolve, reject) => {
-      var max = parseInt(params[0])
-      if (value.length > max) reject(new Error(name, `${name} field must not pass ${max} chars in length`))
-      else resolve()
-    })
-  },
-
-  digits(name, value) {
   }
 }
