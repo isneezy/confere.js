ac362c90465eaccbb76fa4a5c62aff9a338f0b32
isneezy <vilanculoivan@gmail.com>
2017-08-23 10:34:57 +0200

    add bootstrap4 decorator

--- src/decorators/index.js
+++ src/decorators/index.js
@@ -47,3 +47,45 @@ export const bootstrap3 = (results, $form) => {
     error(formGroup($form.elements[name]), $form.elements[name], results[name])
   })
 }
+
+export const bootstrap4 = (results, $form) => {
+  const reset = (element) => {
+    const group = formGroup(element)
+    if(group) {
+      Array.map(group.getElementsByClassName('form-control-feedback'), element => {
+        element.remove()
+      })
+    }
+  }
+  const success = (group, element) => {
+    if (element.type !== 'submit') element.classList.add('form-control-success')
+    group.classList.remove('has-danger')
+    group.classList.add('has-success')
+  }
+
+  const error = (group, element, result) => {
+    element.classList.remove('form-control-success')
+    element.classList.add('form-control-danger')
+    group.classList.remove('has-success')
+    group.classList.add('has-danger')
+    const feedback = document.createElement('span')
+    feedback.classList.add('form-control-feedback')
+    feedback.innerHTML = result[0].message
+    group.appendChild(feedback)
+  }
+
+  const formGroup = (element) => {
+    return parent(element, 'form-group')
+  }
+
+  // add success state for every input
+  for(let element of $form.elements) {
+    reset(element)
+    const group = formGroup(element)
+    if(group) success(group, element)
+  }
+
+  Object.keys(results).map(name => {
+    error(formGroup($form.elements[name]), $form.elements[name], results[name])
+  })
+}
